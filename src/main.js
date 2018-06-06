bcfb9f242c642daae32b5b73deeb5725111150de
wuxun <revolver@126.com>
2018-06-06 23:05:55 +0800

    Add router support

--- src/main.js
+++ src/main.js
@@ -4,19 +4,21 @@ import 'popper.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../web/index.html';
 import Vue from '../node_modules/vue/dist/vue.esm';
-import 'vue-router'
+import VueRouter from 'vue-router';
 
-const NotFound = { template: '<p>Page not found</p>' }
 const Home = { template: '<p>home page</p>' }
 const About = { template: '<p>about page</p>' }
 
-const routes = {
-    '/home': Home,
-    '/about': About
-}
+const routes = [
+    { path: '/home', component: Home },
+    { path: '/about', component: About }
+];
+
 const router = new VueRouter({
     routes
   });
+
+  Vue.use(VueRouter);
 
 var app = new Vue({
     el: '#vue_root',
