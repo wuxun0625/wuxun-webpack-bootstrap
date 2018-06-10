bad5d3b77a8097576ce4868309047e6de5aa9301
wuxun <revolver@126.com>
2018-06-10 22:36:01 +0800

    Add vuetable-2 support

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../web/index.html';
 import Vue from '../node_modules/vue/dist/vue.esm';
 import VueRouter from 'vue-router';
-import home from './view/home.vue'
+import './css/common.css';
+import home from './view/home.vue';
+
 
 const About = { template: '<p>about page</p>' }
 
@@ -16,9 +18,9 @@ const routes = [
 
 const router = new VueRouter({
     routes
-  });
+});
 
-  Vue.use(VueRouter);
+Vue.use(VueRouter);
 
 var app = new Vue({
     el: '#vue_root',
--- src/view/home.vue
+++ src/view/home.vue
@@ -2,21 +2,35 @@
 	<div id="home"
        v-on:click="clickHomeContentFunc">
 		{{WelcomeMsg}}
+
+      <vuetable ref="vuetable"
+                api-url="https://vuetable.ratiw.net/api/users"
+                :fields="['name', 'email', 'birthdate']"
+      class="table table-hover"></vuetable>
+
+
+
 	</div>
 </template>
 <script>
+import vuetable from 'vuetable-2';
+
 export default {
   data() {
     return {
-      WelcomeMsg: 'Welcome to home!'
+      WelcomeMsg: "Welcome to home!"
     };
+  },
+
+  components: {
+    vuetable
   },
 
   props: {},
 
   methods: {
-    clickHomeContentFunc: function () {
-      this.WelcomeMsg = "Click home content"
+    clickHomeContentFunc: function() {
+      this.WelcomeMsg = "Click home content";
     }
   }
 };
