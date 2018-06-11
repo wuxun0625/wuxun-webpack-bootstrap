3d0eebe0d19200ac77035847222c65196c2a3428
xun.wu@oracle.com <xun.wu@oracle.com>
2018-06-11 17:15:31 +0800

    Add support for vuetable-2 paging

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,29 +1,53 @@
 <template>
-	<div id="home"
+  <div id="home"
        v-on:click="clickHomeContentFunc">
-		{{WelcomeMsg}}
+    {{WelcomeMsg}}
 
-      <vuetable ref="vuetable"
-                api-url="https://vuetable.ratiw.net/api/users"
-                :fields="['name', 'email', 'birthdate']"
-      class="table table-hover"></vuetable>
-
-
-
-	</div>
+    <vuetable ref="vuetable"
+              api-url="https://vuetable.ratiw.net/api/users"
+              :fields="fields"
+              pagination-path=""
+              @vuetable:pagination-data="onPaginationData"
+              class="table table-hover"></vuetable>
+    <vuetable-pagination ref="pagination"
+                         @vuetable-pagination:change-page="onChangePage"></vuetable-pagination>
+  </div>
 </template>
 <script>
-import vuetable from 'vuetable-2';
+import vuetable from "vuetable-2";
+import VuetablePagination from "../../node_modules/vuetable-2/src/components/VuetablePagination.vue";
 
 export default {
   data() {
     return {
-      WelcomeMsg: "Welcome to home!"
+      WelcomeMsg: "Welcome to home!",
+      fields: [
+        "name",
+        "email",
+        {
+          name: "birthdate",
+          titleClass: "text-center",
+          dataClass: "text-center"
+        },
+        {
+          name: "nickname"
+        },
+        {
+          name: "gender",
+          titleClass: "center aligned",
+          dataClass: "center aligned",
+          callback: "genderLabel"
+        },
+        {
+          name: "salary"
+        }
+      ]
     };
   },
 
   components: {
-    vuetable
+    vuetable,
+    VuetablePagination
   },
 
   props: {},
@@ -31,6 +55,17 @@ export default {
   methods: {
     clickHomeContentFunc: function() {
       this.WelcomeMsg = "Click home content";
+    },
+    genderLabel(value) {
+      return value === "M"
+        ? '<span class="badge badge-primary">Male</span>'
+        : '<span class="badge badge-danger">Female</span>';
+    },
+    onPaginationData(paginationData) {
+      this.$refs.pagination.setPaginationData(paginationData);
+    },
+    onChangePage(page) {
+      this.$refs.vuetable.changePage(page);
     }
   }
 };
